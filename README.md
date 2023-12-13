840721dfd44c1e17e73795045cd652bfe1cc1511
josephgalloway321 <josephgalloway520@gmail.com>
2023-12-13 10:43:17 -0600

    Animated sesame sitting test

--- README.md
+++ README.md
@@ -4,8 +4,12 @@
 - Another goal was to put into practice the concepts and software design lessons I learned while completing the Pong and Tetris tutorials.
 
 # Notes
-- ...
+- The software used to make the sprites and background is an online pixel editor called Piskel.
+
+# Final Comments
+- This was definitely a challenge for me since I don't focus a lot on artwork aside from music.
 
 # References
-- [Creating Sprite Images for Animations](https://youtu.be/xQk0W7AGh74?si=ELZMXN50-hJN8Kjb) 
 - ["Programming With Nick" VS Code template](https://github.com/educ8s/Raylib-CPP-Starter-Template-for-VSCODE-V2)
+- [Creating Sprite Images for Animations](https://youtu.be/xQk0W7AGh74?si=ELZMXN50-hJN8Kjb) 
+- [Raylib Tutorial | Episode 3 | Drawing textures and animating](https://youtu.be/pFgDWPbUrqk?si=CwrC-XqSSHEXDwpb)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include <raylib.h>
 
-int main()
-{
-  std::cout << "Merp";
-  return 0;
+const int WIDTH = 240, HEIGHT = 240;
+
+int main() {
+  InitWindow(WIDTH, HEIGHT, "Test");
+  SetTargetFPS(60);
+
+  Texture2D sesame = LoadTexture("C:/Users/josep/Documents/GitHub/sesames_quest/resources/sprites/sesame_sitting.png");
+
+  float frame_width = (float)(sesame.width / 6);
+  int max_frames = (int)(sesame.width / (int)frame_width);
+
+  float timer = 0.0f;
+  int frame = 0;
+
+  while(!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(PINK);
+
+    timer += GetFrameTime();  // Time it takes to render a single frame
+    
+    if(timer >= 0.25) {
+      timer = 0.0f;
+      frame += 1;
+    }
+
+    frame = frame % max_frames;
+
+    // Draw the entire Sesame png file
+    //DrawTexture(sesame, 0, 0, WHITE);
+
+    DrawTextureRec(
+            sesame,
+            Rectangle{frame_width * frame, 0, frame_width, (float)sesame.height},
+            Vector2{20, 20},
+            WHITE);
+
+    EndDrawing();
+  }
 }
